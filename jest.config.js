2b08214b6d8acd96697a2b044e8fa3889f82f8d5
zhuangcheng <zhuangcheng@lingzikg.com>
2022-02-17 10:38:11 +0800

    test: (jest)test coverage

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  collectCoverage: true,
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
